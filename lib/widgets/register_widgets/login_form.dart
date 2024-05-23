43faf3cfa736993dd5330365c958c0e60a4c237e
AhmedBoda12 <you@example.com>
2024-05-23 22:47:31 +0300

    completed Auth with firebase

--- lib/widgets/register_widgets/login_form.dart
+++ lib/widgets/register_widgets/login_form.dart
@@ -1,4 +1,6 @@
+import 'package:aroom_pro/views/home_page.dart';
 import 'package:aroom_pro/widgets/register_widgets/social_buttons.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginForm extends StatefulWidget {
@@ -13,6 +15,7 @@ class _LoginFormState extends State<LoginForm> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   bool isScure = true;
+  bool isLoading = false;
 
   @override
   void dispose() {
@@ -27,131 +30,219 @@ class _LoginFormState extends State<LoginForm> {
       padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
       child: Form(
         key: _formKey,
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: double.infinity,
-                child: Text(
-                  'Welcome Back',
-                  textAlign: TextAlign.start,
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ),
-              const SizedBox(
-                height: 4,
-              ),
-              const SizedBox(
-                  width: double.infinity,
-                  child: Text(
-                      'Fill out the information below in order to access your account.')),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                controller: _emailController,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter your email';
-                  } else {
-                    return null;
-                  }
-                },
-                keyboardType: TextInputType.emailAddress,
-                autofillHints: const [AutofillHints.email],
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Theme.of(context).colorScheme.onPrimary,
-                  contentPadding:
-                      const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
-                  labelText: 'Email',
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 2,
-                      color: Colors.black12.withOpacity(0.5),
+        child: isLoading
+            ? const Center(child: CircularProgressIndicator.adaptive())
+            : SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: double.infinity,
+                      child: Text(
+                        'Welcome Back',
+                        textAlign: TextAlign.start,
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
                     ),
-                    borderRadius: BorderRadius.circular(40),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TextFormField(
-                controller: _passwordController,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter your password';
-                  } else {
-                    return null;
-                  }
-                },
-                obscureText: isScure,
-                keyboardType: TextInputType.visiblePassword,
-                autofillHints: const [AutofillHints.password],
-                decoration: InputDecoration(
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        isScure = !isScure;
-                      });
-                    },
-                    icon: isScure
-                        ? const Icon(Icons.visibility_off)
-                        : const Icon(Icons.visibility),
-                  ),
-                  filled: true,
-                  fillColor: Theme.of(context).colorScheme.onPrimary,
-                  contentPadding:
-                      const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
-                  labelText: 'Password',
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 2,
-                      color: Colors.black12.withOpacity(0.5),
+                    const SizedBox(
+                      height: 4,
                     ),
-                    borderRadius: BorderRadius.circular(40),
-                  ),
+                    const SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                            'Fill out the information below in order to access your account.')),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      controller: _emailController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your email';
+                        } else {
+                          return null;
+                        }
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      autofillHints: const [AutofillHints.email],
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Theme.of(context).colorScheme.onPrimary,
+                        contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 24, vertical: 18),
+                        labelText: 'Email',
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            width: 2,
+                            color: Colors.black12.withOpacity(0.5),
+                          ),
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    TextFormField(
+                      controller: _passwordController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your password';
+                        } else {
+                          return null;
+                        }
+                      },
+                      obscureText: isScure,
+                      keyboardType: TextInputType.visiblePassword,
+                      autofillHints: const [AutofillHints.password],
+                      decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              isScure = !isScure;
+                            });
+                          },
+                          icon: isScure
+                              ? const Icon(Icons.visibility_off)
+                              : const Icon(Icons.visibility),
+                        ),
+                        filled: true,
+                        fillColor: Theme.of(context).colorScheme.onPrimary,
+                        contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 24, vertical: 18),
+                        labelText: 'Password',
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            width: 2,
+                            color: Colors.black12.withOpacity(0.5),
+                          ),
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    FilledButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          setState(() {
+                            isLoading = !isLoading;
+                          });
+                          _signInWithEmailAndPassword(_emailController.text,
+                              _passwordController.text, context);
+                        }
+                      },
+                      style: FilledButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 50, vertical: 16),
+                        textStyle: Theme.of(context).textTheme.titleMedium,
+                        elevation: 3,
+                      ),
+                      child: const Text('Get Started'),
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    const Text(
+                      'Or sign up with',
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    const SocialButtons(),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text('Forgot Password?'),
+                    )
+                  ],
                 ),
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              FilledButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                  }
-                },
-                style: FilledButton.styleFrom(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 50, vertical: 16),
-                  textStyle: Theme.of(context).textTheme.titleMedium,
-                  elevation: 3,
-                ),
-                child: const Text('Get Started'),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              const Text(
-                'Or sign up with',
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              const SocialButtons(),
-              TextButton(
-                onPressed: () {},
-                child: const Text('Forgot Password?'),
-              )
-            ],
-          ),
-        ),
       ),
     );
+  }
+
+  void _signInWithEmailAndPassword(
+      String email, String password, BuildContext context) async {
+    try {
+      final user = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      if (context.mounted) {
+        Navigator.pushReplacementNamed(
+          context,
+          HomePage.routeName,
+          arguments: user,
+        );
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        if (context.mounted) {
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: const Text('User not found'),
+              content: const Text(
+                  'No user found for that email.\n Create new account'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    setState(() {
+                      isLoading = !isLoading;
+                    });
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            ),
+          );
+        }
+      } else if (e.code == 'wrong-password') {
+        if (context.mounted) {
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: const Text('Wrong Password'),
+              content: const Text('Wrong password provided for that user.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    setState(() {
+                      isLoading = !isLoading;
+                    });
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            ),
+          );
+        }
+      }
+    } catch (e) {
+      if (context.mounted) {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Error'),
+            content: const Text(''),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  setState(() {
+                    isLoading = !isLoading;
+                  });
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          ),
+        );
+      }
+    }
   }
 }
--- lib/widgets/register_widgets/signup_form.dart
+++ lib/widgets/register_widgets/signup_form.dart
@@ -1,4 +1,6 @@
+import 'package:aroom_pro/views/home_page.dart';
 import 'package:aroom_pro/widgets/register_widgets/social_buttons.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SignupForm extends StatefulWidget {
@@ -13,6 +15,7 @@ class _SignupFormState extends State<SignupForm> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   bool isScure = true;
+  bool isLoading = false;
 
   @override
   void dispose() {
@@ -27,127 +30,219 @@ class _SignupFormState extends State<SignupForm> {
       padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
       child: Form(
         key: _formKey,
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: double.infinity,
-                child: Text(
-                  'Create Account',
-                  textAlign: TextAlign.start,
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ),
-              const SizedBox(
-                height: 4,
-              ),
-              const SizedBox(
-                  width: double.infinity,
-                  child: Text(
-                      'Let\'s get started by filling out the form below.')),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                controller: _emailController,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter your email';
-                  } else {
-                    return null;
-                  }
-                },
-                keyboardType: TextInputType.emailAddress,
-                autofillHints: const [AutofillHints.email],
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Theme.of(context).colorScheme.onPrimary,
-                  contentPadding:
-                      const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
-                  labelText: 'Email',
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 2,
-                      color: Colors.black12.withOpacity(0.5),
+        child: isLoading
+            ? const Center(child: CircularProgressIndicator.adaptive())
+            : SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: double.infinity,
+                      child: Text(
+                        'Create Account',
+                        textAlign: TextAlign.start,
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
                     ),
-                    borderRadius: BorderRadius.circular(40),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TextFormField(
-                controller: _passwordController,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter your password';
-                  } else {
-                    return null;
-                  }
-                },
-                obscureText: isScure,
-                keyboardType: TextInputType.visiblePassword,
-                autofillHints: const [AutofillHints.password],
-                decoration: InputDecoration(
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        isScure = !isScure;
-                      });
-                    },
-                    icon: isScure
-                        ? const Icon(Icons.visibility_off)
-                        : const Icon(Icons.visibility),
-                  ),
-                  filled: true,
-                  fillColor: Theme.of(context).colorScheme.onPrimary,
-                  contentPadding:
-                      const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
-                  labelText: 'Password',
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 2,
-                      color: Colors.black12.withOpacity(0.5),
+                    const SizedBox(
+                      height: 4,
                     ),
-                    borderRadius: BorderRadius.circular(40),
-                  ),
+                    const SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                            'Let\'s get started by filling out the form below.')),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      controller: _emailController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your email';
+                        } else {
+                          return null;
+                        }
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      autofillHints: const [AutofillHints.email],
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Theme.of(context).colorScheme.onPrimary,
+                        contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 24, vertical: 18),
+                        labelText: 'Email',
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            width: 2,
+                            color: Colors.black12.withOpacity(0.5),
+                          ),
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    TextFormField(
+                      controller: _passwordController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your password';
+                        } else {
+                          return null;
+                        }
+                      },
+                      obscureText: isScure,
+                      keyboardType: TextInputType.visiblePassword,
+                      autofillHints: const [AutofillHints.password],
+                      decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              isScure = !isScure;
+                            });
+                          },
+                          icon: isScure
+                              ? const Icon(Icons.visibility_off)
+                              : const Icon(Icons.visibility),
+                        ),
+                        filled: true,
+                        fillColor: Theme.of(context).colorScheme.onPrimary,
+                        contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 24, vertical: 18),
+                        labelText: 'Password',
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            width: 2,
+                            color: Colors.black12.withOpacity(0.5),
+                          ),
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    FilledButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          setState(() {
+                            isLoading = !isLoading;
+                          });
+                          _signUpWithEmailAndPassword(
+                            _emailController.text,
+                            _passwordController.text,
+                            context,
+                          );
+                        }
+                      },
+                      style: FilledButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 50, vertical: 16),
+                        textStyle: Theme.of(context).textTheme.titleMedium,
+                        elevation: 3,
+                      ),
+                      child: const Text('Get Started'),
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    const Text(
+                      'Or sign up with',
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    const SocialButtons()
+                  ],
                 ),
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              FilledButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                  }
-                },
-                style: FilledButton.styleFrom(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 50, vertical: 16),
-                  textStyle: Theme.of(context).textTheme.titleMedium,
-                  elevation: 3,
-                ),
-                child: const Text('Get Started'),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              const Text(
-                'Or sign up with',
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              const SocialButtons()
-            ],
-          ),
-        ),
       ),
     );
+  }
+
+  void _signUpWithEmailAndPassword(
+      String email, String password, BuildContext context) async {
+    try {
+      final user = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      if (context.mounted) {
+        Navigator.pushReplacementNamed(
+          context,
+          HomePage.routeName,
+          arguments: user,
+        );
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        if (context.mounted) {
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: const Text('Weak Password'),
+              content: const Text('The password provided is too weak.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    setState(() {
+                      isLoading = !isLoading;
+                    });
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            ),
+          );
+        }
+      } else if (e.code == 'email-already-in-use') {
+        if (context.mounted) {
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: const Text('Email already in use'),
+              content: const Text('The account already exists for that email.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    setState(() {
+                      isLoading = !isLoading;
+                    });
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            ),
+          );
+        }
+      }
+    } catch (e) {
+      if (context.mounted) {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Error'),
+            content: const Text(''),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  setState(() {
+                    isLoading = !isLoading;
+                  });
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          ),
+        );
+      }
+    }
   }
 }
