3872a382a87cb030ade279db256fa3c4aff1b4b3
AhmedBoda12 <you@example.com>
2024-06-28 03:29:03 +0300

    manage empty products

--- lib/widgets/product_widgets/products_gridview.dart
+++ lib/widgets/product_widgets/products_gridview.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:aroom_pro/models/product_model.dart';
 import 'package:aroom_pro/widgets/empty_widget.dart';
 import 'package:aroom_pro/widgets/product_widgets/product_card.dart';
@@ -15,6 +13,36 @@ class ProductsGridview extends StatelessWidget {
   final String? categoryId;
   @override
   Widget build(BuildContext context) {
+    if (products.isEmpty) {
+      return const EmptyWidget(
+          text: 'There is no products', image: 'assets/aroom_logo.png');
+    } else {
+      List<ProductModel> filteredProducts =
+          products.where((e) => e.categoryId == categoryId!).toList();
+      if (categoryId == 'b4erAEHt6SQ2ZyklR3H4') {
+        return ProductGridviewBuilder(
+          products: products,
+        );
+      } else if (filteredProducts.isEmpty) {
+        return const EmptyWidget(
+            text: 'There is no products', image: 'assets/aroom_logo.png');
+      } else {
+        return ProductGridviewBuilder(products: filteredProducts);
+      }
+    }
+  }
+}
+
+class ProductGridviewBuilder extends StatelessWidget {
+  const ProductGridviewBuilder({
+    super.key,
+    required this.products,
+  });
+
+  final List<ProductModel> products;
+
+  @override
+  Widget build(BuildContext context) {
     return GridView.builder(
       physics: const BouncingScrollPhysics(),
       padding: const EdgeInsets.symmetric(horizontal: 8),
@@ -27,27 +55,7 @@ class ProductsGridview extends StatelessWidget {
       ),
       itemCount: products.length,
       itemBuilder: (context, index) {
-        List<ProductModel> filteredProducts = [];
-        if (categoryId != null) {
-          if (categoryId == 'b4erAEHt6SQ2ZyklR3H4') {
-            return ProductCard(
-              product: products[index],
-            );
-          }
-          filteredProducts =
-              products.where((e) => e.categoryId == categoryId!).toList();
-          log(filteredProducts.toString());
-          if (filteredProducts.isEmpty) {
-            return const EmptyWidget(
-                text: 'No products', image: 'assets/aroom_logo.png');
-          } else {
-            return ProductCard(product: filteredProducts[index]);
-          }
-        } else {
-          return ProductCard(
-            product: products[index],
-          );
-        }
+        return ProductCard(product: products[index]);
       },
     );
   }
